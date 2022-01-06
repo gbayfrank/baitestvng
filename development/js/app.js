7f7c41e27ea7781ab06202e3bdde82a17f74d1f1
gbayfrank <vuducthao2020@gmail.com>
2022-01-06 10:28:19 +0700

    Chuyen tu webpack sang Gruntjs

--- development/js/app.js
+++ development/js/app.js
@@ -1,10 +1,7 @@
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-
 // loads the Icon plugin
-UIkit.use(Icons);
+// UIkit.use(Icons);
 
 // components can be called from the imported UIkit reference
-// UIkit.notification('Hello world.');
+UIkit.notification('Hello world.');
 
-// console.log('Xin chào thao 123!!');
+console.log('Xin chào thao 123!!');
